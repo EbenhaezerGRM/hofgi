a31300f11033190ac04b6210cdebe145dd45b9b5
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2022-08-18 23:46:34 +0700

    Updates

---
+++ precache-manifest.2cb4ab86a78e74a7b05c0cd1e95f7fef.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cb87caeb9550aab8968",
+    "url": "/hofgi/static/css/main.58ce2e50.chunk.css"
+  },
+  {
+    "revision": "5cb87caeb9550aab8968",
+    "url": "/hofgi/static/js/main.5cb87cae.chunk.js"
+  },
+  {
+    "revision": "2e8f1a6b9f28a667d85d",
+    "url": "/hofgi/static/js/1.2e8f1a6b.chunk.js"
+  },
+  {
+    "revision": "d06d64c7e63a97acde46",
+    "url": "/hofgi/static/css/2.39e9723e.chunk.css"
+  },
+  {
+    "revision": "d06d64c7e63a97acde46",
+    "url": "/hofgi/static/js/2.d06d64c7.chunk.js"
+  },
+  {
+    "revision": "ebf8010b6a66ae34d639",
+    "url": "/hofgi/static/js/runtime~main.ebf8010b.js"
+  },
+  {
+    "revision": "b3802fd1d44f7f4d2e14e980db741eb6",
+    "url": "/hofgi/static/media/Ganyu1.b3802fd1.jpeg"
+  },
+  {
+    "revision": "bdd8307d9fc81c844c407bf81945c070",
+    "url": "/hofgi/static/media/Ayaka.bdd8307d.jpg"
+  },
+  {
+    "revision": "0dc22386913ac5653276e27977b1a43e",
+    "url": "/hofgi/static/media/Ei2.0dc22386.jpg"
+  },
+  {
+    "revision": "a2dfb2dc6f99ffbdf063e83ef5323d3d",
+    "url": "/hofgi/static/media/Yaemiko1.a2dfb2dc.jpeg"
+  },
+  {
+    "revision": "b5e080abda935fa370474e718cafad29",
+    "url": "/hofgi/static/media/Lumine1.b5e080ab.jpg"
+  },
+  {
+    "revision": "9a3c0cd6da10adca90c07163a8661347",
+    "url": "/hofgi/static/media/JadeCutter.9a3c0cd6.jpg"
+  },
+  {
+    "revision": "5d85c4dc9da82d7f9b2daef5825b0fe1",
+    "url": "/hofgi/static/media/Mistsplitter1.5d85c4dc.png"
+  },
+  {
+    "revision": "48eb51c548bd809235ad4f61cd2f7214",
+    "url": "/hofgi/static/media/Ayatoless.48eb51c5.png"
+  },
+  {
+    "revision": "6bc1158314efe61bdb5e06465fb4be13",
+    "url": "/hofgi/static/media/Ganyu_Land.6bc11583.png"
+  },
+  {
+    "revision": "380212593eaf7711463413a9daf6f52a",
+    "url": "/hofgi/static/media/bggi.38021259.jpg"
+  },
+  {
+    "revision": "ee489fab16138d26674ab725ebc9fb27",
+    "url": "/hofgi/index.html"
+  }
+];
